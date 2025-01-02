659d8e855ed81e8072457999772b13c00811e2b9
Dil Bahadur Tharu <dtharu486@gmail.com>
2025-01-02 16:34:34 +0545

    complete login,logout and refresh,access token

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,21 +1,27 @@
-import {Router} from 'express';
-import { userRegister } from '../controllers/userControllers.js';
-import{upload} from '../middlewares/multerMiddlerwares.js';
-
+import { Router } from 'express';
+import { loginUser, logoutUser, refreshAccessToken, userRegister } from '../controllers/userControllers.js';
+import { upload } from '../middlewares/multerMiddlerware.js';
+import { verifyJWT } from '../middlewares/authMiddleware.js';
 
 const router = Router();
 
 router.route('/register').post(
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1
-        }, 
-        {
-            name: "coverImage",
-            maxCount: 1
-        }
-    ]),
-    userRegister);
+  upload.fields([
+    {
+      name: 'avatar',
+      maxCount: 1,
+    },
+    {
+      name: 'coverImage',
+      maxCount: 1,
+    },
+  ]),
+  userRegister
+);
+router.route('/login').post(loginUser);
+
+//secured routes
+router.route('/logout').post(verifyJWT, logoutUser);
+router.route('/refresh-token').post(refreshAccessToken);
 
 export default router;
